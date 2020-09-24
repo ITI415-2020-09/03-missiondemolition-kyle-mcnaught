7a1b838ca6217fa4cad5ccff6f1f8a0e2338f392
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-09-24 17:58:42 -0400

    Getting Projectile to launch
    Made it so the projectiles launch out of the slingshot.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -42,22 +42,22 @@ public GameObject         launchPoint;
         // Start it at the launchPoint
         projectile.transform.position = launchPos;
         // Set it to isKinematic for now
-        projectileRigidbody = projectile.GetComponent<Rigidbody>();                // a
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();                
         projectileRigidbody.isKinematic = true;
     }
     
     void Update() {
         // If Slingshot is not in aimingMode, don't run this code
-        if (!aimingMode) return;                                                   // b
+        if (!aimingMode) return;                                                   
 
         // Get the current mouse position in 2D screen coordinates
-        Vector3 mousePos2D = Input.mousePosition;                                  // c
+        Vector3 mousePos2D = Input.mousePosition;                                  
         mousePos2D.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
 
         // Find the delta from the launchPos to the mousePos3D
         Vector3 mouseDelta = mousePos3D-launchPos;
-        // Limit mouseDelta to the radius of the Slingshot SphereCollider          // d
+        // Limit mouseDelta to the radius of the Slingshot SphereCollider          
         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
         if (mouseDelta.magnitude > maxMagnitude) {
             mouseDelta.Normalize();
@@ -68,7 +68,7 @@ public GameObject         launchPoint;
         Vector3 projPos = launchPos + mouseDelta;
         projectile.transform.position = projPos;
 
-        if ( Input.GetMouseButtonUp(0) ) {                                         // e
+        if ( Input.GetMouseButtonUp(0) ) {                                         
             // The mouse has been released
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
