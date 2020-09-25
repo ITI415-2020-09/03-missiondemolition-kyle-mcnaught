32c6d23af6cab7d94809de76924568a7c3f14c08
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-09-24 21:05:51 -0400

    Added several clouds
    Several clouds now load behind the scene.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -48,9 +48,7 @@ public class Cloud : MonoBehaviour
       
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space)) {                                         
-            Restart();
-        }  
+      
     }
     
     void Restart() {                                                                   
